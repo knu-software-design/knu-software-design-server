11c073b7672b664f116d21a02271c5df824c7424
GyunHee <ef3232@naver.com>
2025-12-01 16:11:36 +0900

    feat: base엔티티 추가

--- src/main/java/sw2/project/ProjectApplication.java
+++ src/main/java/sw2/project/ProjectApplication.java
@@ -2,7 +2,9 @@ package sw2.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectApplication {
 
