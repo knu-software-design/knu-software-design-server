bde36a3a80c6a77401e898deb5a5c009caf65cf7
GyunHee <ef3232@naver.com>
2025-12-01 16:12:35 +0900

    feat: 질문 DTO 추가

---
+++ src/main/java/sw2/project/feature/qa/dto/QuestionResponse.java
@@ -0,0 +1,30 @@
+package sw2.project.feature.qa.dto;
+
+import lombok.Getter;
+import sw2.project.domain.question.Question;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class QuestionResponse {
+
+    private final Long questionId;
+    private final String title;
+    private final String content;
+    private final String authorId;
+    private final LocalDateTime createdAt;
+    private final List<AnswerResponse> answers;
+
+    public QuestionResponse(Question question) {
+        this.questionId = question.getId();
+        this.title = question.getTitle();
+        this.content = question.getContent();
+        this.authorId = question.getAuthor().getId();
+        this.createdAt = question.getCreatedAt();
+        this.answers = question.getAnswers().stream()
+                .map(AnswerResponse::new)
+                .collect(Collectors.toList());
+    }
+}
