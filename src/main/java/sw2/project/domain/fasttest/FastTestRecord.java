df76afabce9d9540ed034ca2cef2c1aae4a673dc
GyunHee <ef3232@naver.com>
2025-12-02 13:06:23 +0900

    feat: 자가진단 FastTest엔티티 추가

---
+++ src/main/java/sw2/project/domain/fasttest/FastTestRecord.java
@@ -0,0 +1,32 @@
+package sw2.project.domain.fasttest;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sw2.project.common.BaseTimeEntity;
+
+@Entity
+@Table(name = "fast_test_records")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FastTestRecord extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 36)
+    private String userId;
+
+    @Lob
+    @Column(columnDefinition = "JSON")
+    private String result;
+
+    @Builder
+    public FastTestRecord(String userId, String result) {
+        this.userId = userId;
+        this.result = result;
+    }
+}
---
+++ src/main/java/sw2/project/domain/fasttest/FastTestRecordRepository.java
@@ -0,0 +1,6 @@
+package sw2.project.domain.fasttest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FastTestRecordRepository extends JpaRepository<FastTestRecord, Long> {
+}
